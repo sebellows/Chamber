06048338240b94dc1b3e83853befbde66383c19c
Sean Bellows <seanbellows@gmail.com>
2016-10-01 00:57:48 -0400

    fix sidebars and rearrange their names so the -sidebar is a suffix

--- landing-page.php
+++ landing-page.php
@@ -7,22 +7,30 @@
 <?php get_template_part('templates/content', 'landing'); ?>
 
 <?php
-	if ( is_page( 'home' ) ) {
-		'<aside class="sidebar sidebar-primary">' . dynamic_sidebar('sidebar-primary') . '</aside>';
-	}
-	if ( is_page( 'cvb' ) ) {
-		'<aside class="sidebar cvb-sidebar">' . dynamic_sidebar('cvb-sidebar') . '</aside>';
-	}
-    if ( is_page( 'economic-development' ) ) {
-    	'<aside class="sidebar development-sidebar">' . dynamic_sidebar('development-sidebar') . '</aside>';
+    $slug = ! is_front_page() || is_home() ? basename(get_permalink()) : 'primary';
+?>
+
+<section class="sidebar sidebar-<?php echo $slug ?>">
+
+<?php
+    if ( is_front_page() ) {
+        dynamic_sidebar('sidebar-primary');
     }
-    if ( is_page( 'education-training' ) ) {
-    	'<aside class="sidebar education-training-sidebar">' . dynamic_sidebar('education-training-sidebar') . '</aside>';
+    elseif ( is_page( 'cvb' ) ) {
+        dynamic_sidebar('sidebar-cvb');
     }
-    if ( is_page( 'member-services' ) ) {
-    	'<aside class="sidebar member-services-sidebar">' . dynamic_sidebar('member-services-sidebar') . '</aside>';
+    elseif ( is_page( 'economic-development' ) ) {
+        dynamic_sidebar('sidebar-economic-development');
     }
-    if ( is_page( 'shared-services' ) ) {
-    	'<aside class="sidebar shared-services-sidebar">' . dynamic_sidebar('shared-services-sidebar') . '</aside>';
+    elseif ( is_page( 'education-training' ) ) {
+        dynamic_sidebar('sidebar-education-training');
+    }
+    elseif ( is_page( 'member-services' ) ) {
+        dynamic_sidebar('sidebar-member-services');
+    }
+    elseif ( is_page( 'shared-services' ) ) {
+        dynamic_sidebar('sidebar-shared-services');
     }
 ?>
+
+</section>
