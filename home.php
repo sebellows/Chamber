e62252d85d6cc8a6196375edf004ef92974f64f1
Sean Bellows <seanbellows@gmail.com>
2016-10-03 04:25:27 -0400

    add home.php template for news page

---
+++ home.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying indexed posts.
+ * Used for the News Room.
+ *
+ * @link https://developer.wordpress.org/themes/basics/#home-page-display
+ *
+ * @package chamber
+ */
+?>
+
+<?php get_template_part( 'templates/sections/section', 'featured-posts' ); ?>
+
+<div class="sleeve">
+	<div id="news-feed" class="compartment news-feed">
+
+		<h2 class="feed-title">Recent Stories</h2>
+	
+		<?php
+			while (have_posts()) : the_post();
+				get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format());
+			endwhile;
+		?>
+	</div><!-- .news-feed -->
+	
+	<aside id="news-sidebar" class="sidebar news-sidebar" role="complementary">
+		<?php dynamic_sidebar( 'sidebar-news' ); ?>
+	</aside><!-- #news-sidebar -->
+
+</div>
+
+<?php #get_template_part('templates/content-single', get_post_type()); ?>
---
+++ templates/sections/section-featured-posts.php
@@ -0,0 +1,60 @@
+<?php
+
+use Chamber\Titles;
+
+$args = array(
+    'posts_per_page' => 3,
+    'meta_key' => 'featured-post-meta',
+    'meta_value' => 'yes'
+);
+$featured = new WP_Query($args);
+
+if ($featured->have_posts()) : 
+
+    // Count the number of Duplos (start at '1' in order to count header)
+    $counter = 1;
+
+    while($featured->have_posts()) : $featured->the_post();
+        $counter++;
+    endwhile;
+
+?>
+
+<section class="duplo-set">
+
+    <div class="duplo-blocks news-duplo-blocks" m-DuploCount="<?php echo $counter; ?>">
+
+        <header class="duplo news-header" m-Duplo="1">
+            <h1><?= Titles\title(); ?></h1>
+        </header>
+
+        <?php
+            while($featured->have_posts()) : $featured->the_post();
+
+            $index = $featured->current_post + 2;
+        ?>
+
+        <div class="duplo" m-Duplo="<?php echo $index; ?>">
+            <?php if ( has_post_thumbnail())  : ?>
+                <?php the_post_thumbnail( 'full', [ 'class' => 'duplo-image' ] ); ?>
+            <?php endif; ?>
+
+            <div class="duplo-skrim" aria-hidden="true"></div>
+
+            <div class="duplo-content">
+                <time><?php echo get_the_date('F j, Y'); ?></time>
+                <h2><?php the_title(); ?></h2>
+                <p><?php the_excerpt();?></p>
+            </div>
+
+            <a href="<?php the_permalink(); ?>" class="duplo-link"></a>
+        </div>
+
+        <?php endwhile; ?>
+
+    </div>
+
+</section>
+
+<?php endif; ?>
+
