3bf9e84aea42abd00a3297bd89774a457b148ed4
Sean Bellows <seanbellows@gmail.com>
2016-10-17 15:41:14 -0400

    remove duplicate paging navigation from post footer

--- home.php
+++ home.php
@@ -21,8 +21,6 @@ use Chamber\Theme\TemplateTags;
 		<?php while (have_posts()) : the_post(); ?>
 			<?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 		<?php endwhile; ?>		
-		
-		<?php TemplateTags\post_pagination(); // Previous/next page navigation. ?>
 	</div>
 
 </div><!-- .news-feed -->
