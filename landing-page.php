aacadc5530527e0a6d128d22948ae4ef7c9bce71
Sean Bellows <seanbellows@gmail.com>
2016-11-07 17:48:23 -0500

    add conditional statement to prevent content from displaying above landing-page content if duplo_block section is not present

--- landing-page.php
+++ landing-page.php
@@ -5,10 +5,12 @@
 
 ?>
 
-<div class="row">
-	<?php while (have_posts()) : the_post(); ?>
-		<?php get_template_part('templates/content/page'); ?>
-	<?php endwhile; ?>
-</div>
+<?php if ( have_rows('duplo_block') ) : ?>
+	<div class="row">
+		<?php while (have_posts()) : the_post(); ?>
+			<?php get_template_part('templates/content/page'); ?>
+		<?php endwhile; ?>
+	</div>
+<?php endif; ?>
 
 <?php get_template_part('templates/sections/section', 'landing-page'); ?>
