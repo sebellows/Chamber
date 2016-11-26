2d5b898311ddf7d9e6613abad84cc620f4606799
Joel Howard <joelhowa@umflint.edu>
2016-11-26 10:49:03 -0500

    removed pagination from attractions page

--- menu/loop-nav.php
+++ menu/loop-nav.php
@@ -2,7 +2,7 @@
 
 if ( is_home() || is_archive() || is_search()) {
     // don't display pagination for people
-    if(!is_post_type_archive('person')) {
+    if(!is_post_type_archive('person') && !is_post_type_archive('attraction')) {
         \Chamber\Theme\TemplateTags\post_pagination();
     }
 }
