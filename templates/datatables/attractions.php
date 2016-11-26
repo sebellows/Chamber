8fde39976f4cc8406dd9dd406d7472ccd1832bef
Joel Howard <joelhowa@umflint.edu>
2016-11-25 22:27:50 -0500

    updated query for fetching cities on attractions datatable.  was querying all copies of posts instead of jsut the active(publish)

--- templates/datatables/attractions.php
+++ templates/datatables/attractions.php
@@ -8,104 +8,107 @@
 use Chamber\Theme\Helper;
 global $wpdb;
 
-    // get our attractions
-    $attractions = new WP_Query(array(
-        'posts_per_page'   => -1, // get all for now... add pagination later
-        'post_type'        => 'attraction',
-        'suppress_filters' => true
-    ));
+// get our attractions
+$attractions = new WP_Query(array(
+    'posts_per_page'   => -1, // get all for now... add pagination later
+    'post_type'        => 'attraction',
+    'suppress_filters' => true
+));
 
-    // get all our types for filters
-    $attractionTypes = get_terms('attraction_category');
+// get all our types for filters
+$attractionTypes = get_terms('attraction_category');
 
-    // get all of our cities... this should be good!
-    $attractionCities = $wpdb->get_results("SELECT DISTINCT meta_value FROM wp_postmeta WHERE meta_key='attr_city' ORDER BY meta_value ASC;");
-?>
+// get all of our cities... this should be good!
+$attractionCities = $wpdb->get_results("SELECT DISTINCT wp_postmeta.meta_value FROM wp_postmeta
+    LEFT JOIN wp_posts ON wp_postmeta.post_id = wp_posts.ID
+    WHERE wp_postmeta.meta_key='attr_city' AND  wp_posts.post_status='publish'
+    ORDER BY meta_value ASC;");
+    ?>
 
-<div class="data-grid full-screen-grid">
+    <div class="data-grid full-screen-grid">
 
-    <div class="row column">
-        <?php
+        <div class="row column">
+            <?php
             if ( have_posts() ) :
                 while ( have_posts() ) :
                     the_post();
                     the_content();
                 endwhile;
-            ?>
-            <hr />
-        <?php endif; ?>
-        <div class='filter-buttons'>
-            <h3>Cities</h3>
-            <?php foreach($attractionCities as $attractionCity): ?>
-                <button type="button" class="small hollow button" data-filter='city' data-value="<?= $attractionCity->meta_value; ?>"><?= $attractionCity->meta_value; ?></button>
-            <?php endforeach; ?>
-            <button type="button" class="small secondary button" data-clear='city'>Clear City Filter</button>
+                ?>
+                <hr />
+            <?php endif; ?>
+            <div class='filter-buttons'>
+                <h3>Cities</h3>
+                <?php foreach($attractionCities as $attractionCity): ?>
+                    <button type="button" class="small hollow button" data-filter='city' data-value="<?= $attractionCity->meta_value; ?>"><?= $attractionCity->meta_value; ?></button>
+                <?php endforeach; ?>
+                <button type="button" class="small secondary button" data-clear='city'>Clear City Filter</button>
 
-            <h3>Types</h3>
-            <?php foreach($attractionTypes as $attractionType): ?>
-                <button type="button" class="small hollow button" data-filter='type' data-value="<?= $attractionType->name; ?>"><?= $attractionType->name; ?></button>
-            <?php endforeach; ?>
-            <button type="button" class="small secondary button" data-clear='type'>Clear Type Filter</button>
-            <hr />
+                <h3>Types</h3>
+                <?php foreach($attractionTypes as $attractionType): ?>
+                    <button type="button" class="small hollow button" data-filter='type' data-value="<?= $attractionType->name; ?>"><?= $attractionType->name; ?></button>
+                <?php endforeach; ?>
+                <button type="button" class="small secondary button" data-clear='type'>Clear Type Filter</button>
+                <hr />
 
-        </div>
-        <?php if($attractions->have_posts()): ?>
-            <table id="attractionsDataTable" class="table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Type</th>
-                        <th>City</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php while($attractions->have_posts()): $attractions->the_post();  ?>
+            </div>
+            <?php if($attractions->have_posts()): ?>
+                <table id="attractionsDataTable" class="table">
+                    <thead>
                         <tr>
-                            <td>
-                                <?php if ( has_post_thumbnail() ) : ?>
+                            <th>Name</th>
+                            <th>Type</th>
+                            <th>City</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while($attractions->have_posts()): $attractions->the_post();  ?>
+                            <tr>
+                                <td>
+                                    <?php if ( has_post_thumbnail() ) : ?>
 
-                                    <?php get_the_image(
-                                        [
-                                            'size'         => 'thumbnail',
-                                            'order'        => [ 'featured' ],
-                                            'link'         => true
-                                        ]
-                                    ); ?>
+                                        <?php get_the_image(
+                                            [
+                                                'size'         => 'thumbnail',
+                                                'order'        => [ 'featured' ],
+                                                'link'         => true
+                                            ]
+                                        ); ?>
 
-                                <?php endif; ?>
+                                    <?php endif; ?>
 
-                                <h3 itemprop="headline"><a href="<?php esc_url(the_permalink()); ?>"><?php the_title(); ?></a></h3>
+                                    <h3 itemprop="headline"><a href="<?php esc_url(the_permalink()); ?>"><?php the_title(); ?></a></h3>
 
-                                <?php
-                        			$excerpt = has_excerpt() ? get_the_excerpt() : get_the_content();
-                        			$excerpt = Helper::limit_content($excerpt, 150, '<a class="continuedmark" href="' . esc_url( get_permalink() ) . '">&nbsp;&hellip;MORE</a>');
-                    			?>
+                                    <?php
+                                    $excerpt = has_excerpt() ? get_the_excerpt() : get_the_content();
+                                    $excerpt = Helper::limit_content($excerpt, 150, '<a class="continuedmark" href="' . esc_url( get_permalink() ) . '">&nbsp;&hellip;MORE</a>');
+                                    ?>
 
-                                <p <?php hybrid_attr( 'entry-summary' ); ?>>
-                                    <?= $excerpt; ?>
-                                </p>
-                            </td>
-                            <td>
-                                <?php
+                                    <p <?php hybrid_attr( 'entry-summary' ); ?>>
+                                        <?= $excerpt; ?>
+                                    </p>
+                                </td>
+                                <td>
+                                    <?php
                                     $categories = get_the_terms( $post->ID, 'attraction_category' );
                                     if($categories):
                                         $prefix = '';
                                         foreach ( $categories as $category):
-                                           //echo $prefix . '<a href="/attractions/' . $category->slug .'">' . $category->name . '</a>'; ~ until we figure out how to display these
-                                           echo $prefix . '<a href="#">' . $category->name . '</a>';
-                                           $prefix = ', ';
+                                            //echo $prefix . '<a href="/attractions/' . $category->slug .'">' . $category->name . '</a>'; ~ until we figure out how to display these
+                                            echo $prefix . '<a href="#">' . $category->name . '</a>';
+                                            $prefix = ', ';
                                         endforeach;
                                     endif;
-                                 ?>
-                             </td>
-                            <td>
-                                <?= get_field('attr_city'); ?>
-                            </td>
-                        </tr>
+                                    ?>
+                                </td>
+                                <td>
+                                    <?= get_field('attr_city'); ?>
+                                </td>
+                            </tr>
 
-                    <?php endwhile; ?>
-                </tbody>
-            </table>
-        <?php endif; ?>
-    </div>
-</div><!-- .data-grid -->
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+            <?php endif; ?>
+        </div>
+    </div><!-- .data-grid -->
