c038a4ca6344c9bb16139a10bbd7db43c36a635e
Joel Howard <joelhowa@umflint.edu>
2017-05-07 22:58:40 -0400

    added link to attractions lead
    added link to datatables full listing and added css styling to add underline/bold to make link stand out.

--- archive-attraction.php
+++ archive-attraction.php
@@ -9,12 +9,13 @@
 
 // categories to pull from
 $attractionCategories = array(
-    'arts-and-culture',
-    'dining',
-    'indoor-recreation',
-    'lodging',
-    'outdoor-recreation',
-    'shopping'
+	'arts-and-culture',
+	'dining',
+	'indoor-recreation',
+	'lodging',
+	'outdoor-recreation',
+	'shopping',
+	'local-events'
 );
 
 // number of attractions per category
@@ -22,70 +23,69 @@ $numAttractions = 12;
 
 // our initial query for finding featured categories
 $initialParams = array(
-    'post_type'           => 'attraction',
-    'orderby'             => 'rand',
-    'attraction_category' => '',
-    'posts_per_page'      => 12,
-    'meta_query'          => array(
-        'relation' => 'AND',
-        array(
-            'key'     => 'attr_featured',
-            'value'   => '1',
-            'compare' => '='
-        )
-    )
+	'post_type'           => 'attraction',
+	'orderby'             => 'rand',
+	'attraction_category' => '',
+	'posts_per_page'      => 12,
+	'meta_query'          => array(
+		'relation' => 'AND',
+		array(
+			'key'     => 'attr_featured',
+			'value'   => '1',
+			'compare' => '='
+		)
+	)
 );
 
 $attractionsToDisplay = array();
 
-function loopAttractions($attractions)
-{
-    if ($attractions->have_posts()) {
-        while ($attractions->have_posts()) : $attractions->the_post();
-            get_template_part('templates/archive/attraction');
-        endwhile;
-    }
+function loopAttractions( $attractions ) {
+	if ( $attractions->have_posts() ) {
+		while ( $attractions->have_posts() ) : $attractions->the_post();
+			get_template_part( 'templates/archive/attraction' );
+		endwhile;
+	}
 }
 
 ?>
 
 <div id="app" class="isotope-archive">
-    <?php get_template_part('menu/isotope'); ?>
+	<?php get_template_part( 'menu/isotope' ); ?>
     <div class="card-grid">
 
-        <?php
-        foreach ($attractionCategories as $attractionCategory) {
-            $initialParams['attraction_category'] = $attractionCategory;
-            $initialResults = new WP_Query($initialParams);
+		<?php
+		foreach ( $attractionCategories as $attractionCategory ) {
+			$initialParams['attraction_category'] = $attractionCategory;
+			$initialResults                       = new WP_Query( $initialParams );
 
-            loopAttractions($initialResults);
+			loopAttractions( $initialResults );
 
-            if ($initialResults->post_count < $numAttractions) {
-                // get all our post ids... i'm sure there is an easier way
-                $initialAttractionIds = wp_list_pluck($initialResults->get_posts(), 'ID');
-                $resultsLimit = $numAttractions - $initialResults->post_count;
+			if ( $initialResults->post_count < $numAttractions ) {
+				// get all our post ids... i'm sure there is an easier way
+				$initialAttractionIds = wp_list_pluck( $initialResults->get_posts(), 'ID' );
+				$resultsLimit         = $numAttractions - $initialResults->post_count;
 
 
-                $fillerParams = array(
-                    'post_type'           => 'attraction',
-                    'orderby'             => 'rand',
-                    'post__not_in'        => $initialAttractionIds,
-                    'attraction_category' => $attractionCategory,
-                    'posts_per_page'      => $resultsLimit,
-                    'meta_query'          => array(
-                        array('key' => '_thumbnail_id')
-                    )
+				$fillerParams = array(
+					'post_type'           => 'attraction',
+					'orderby'             => 'rand',
+					'post__not_in'        => $initialAttractionIds,
+					'attraction_category' => $attractionCategory,
+					'posts_per_page'      => $resultsLimit,
+					'meta_query'          => array(
+						array( 'key' => '_thumbnail_id' )
+					)
 
-                );
+				);
 
-                $fillerResults = new WP_Query($fillerParams);
+				$fillerResults = new WP_Query( $fillerParams );
 
-                loopAttractions($fillerResults);
-            }
+				loopAttractions( $fillerResults );
+			}
 
-            wp_reset_postdata();
-        }
+			wp_reset_postdata();
+		}
 
-        ?>
+		?>
     </div>
 </div><!-- .isotope-archive -->
