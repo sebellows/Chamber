915b44e868aa34f0468660494c18f1dc96914880
Joel Howard <joelhowa@umflint.edu>
2017-05-07 22:59:07 -0400

    production gulp compress/compile for attractions link

--- menu/isotope.php
+++ menu/isotope.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Template part for displaying Attractions posts in an Isotope application.
- * 
+ *
  * @package chamber
  */
 
@@ -28,50 +28,56 @@ function chamber_term_icon( $term ) {
 	}
 }
 
-if ( taxonomy_exists('attraction_category') ) :
+if ( taxonomy_exists( 'attraction_category' ) ) :
 
 	$args = [
-	    'taxonomy'      => 'attraction_category',
-	    'orderby'		=> 'id',
-	    'parent'        => 0,
-	    'child_of'		=> 0,
-	    'number'        => 6,
-	    'hide_empty'    => 0
+		'taxonomy'   => 'attraction_category',
+		'orderby'    => 'id',
+		'parent'     => 0,
+		'child_of'   => 0,
+		'number'     => 6,
+		'hide_empty' => 0
 	];
 
 	$terms = get_terms( $args );
 
-?>
+	?>
 
-<header class="isotope-header" m-UI="cvb">
+    <header class="isotope-header" m-UI="cvb">
 
-	<div class="isotope-hgroup">
-		<h1>Things to do in <wbr>Flint&nbsp;&amp;&nbsp;Genesee</h1>
-		
-		<p class="lead">There is a lot to explore, see, and do here. You can filter activities by what you want to do or see them all!</p>
-	</div>
+        <div class="isotope-hgroup">
+            <h1>Things to do in
+                <wbr>
+                Flint&nbsp;&amp;&nbsp;Genesee
+            </h1>
 
-	<menu class="isotope-sortable-menu">
-		<?php foreach($terms as $term) { ?>
-		<a href="#" data-filter=".<?= $term->taxonomy . '-' . $term->slug; ?>">
+            <p class="lead">
+                There is a lot to explore, see, and do here.<br/>
+                You can filter activities by what you want to do, see them all, or view a <a href="<?php echo site_url( 'visit' ); ?>" alt="Complete listing of attractions">complete listing</a>!
+            </p>
+        </div>
+
+        <menu class="isotope-sortable-menu">
+			<?php foreach ( $terms as $term ) { ?>
+                <a href="#" data-filter=".<?= $term->taxonomy . '-' . $term->slug; ?>">
 			<span class="icon" m-Icon="large">
 				<svg role="presentation" viewbox="0 0 32 32">
-					<use xlink:href="#icon-<?= chamber_term_icon($term->slug); ?>"></use>
+					<use xlink:href="#icon-<?= chamber_term_icon( $term->slug ); ?>"></use>
 				</svg>
 			</span>
-			<?= $term->name; ?>
-		</a>
-		<?php } ?>
-		<a href="#" data-filter="*" class="is-selected">
+					<?= $term->name; ?>
+                </a>
+			<?php } ?>
+            <a href="#" data-filter="*" class="is-selected">
 			<span class="icon" m-Icon="large">
 				<svg role="presentation" viewbox="0 0 32 32">
 					<use xlink:href="#icon-bullet-list"></use>
 				</svg>
 			</span>
-			All
-		</a>
-	</menu>
+                All
+            </a>
+        </menu>
 
-</header> <!-- end isotope-header -->
+    </header> <!-- end isotope-header -->
 
 <?php endif; ?>
