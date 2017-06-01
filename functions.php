50a32d5b863e32ecd3b43e6390e0535b7bb3d5b0
Joel Howard <joelhowa@umflint.edu>
2017-05-31 22:38:15 -0400

    added filter for shortcodes to be used in widgets

--- functions.php
+++ functions.php
@@ -4,13 +4,17 @@ namespace Chamber\Theme;
 
 use Chamber\Theme\Theme;
 
-if ( file_exists( __DIR__ . '/vendor/autoload.php' ) )
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
+}
 
 global $chamber_theme;
 
 $chamber_theme = Theme::getInstance();
 // dd($chamber_theme);
 $chamber_theme->boot();
+
+# Enable shortcodes in text widgets - Joel
+add_filter( 'widget_text', 'do_shortcode' );
 
 // dd(chamber('bootstrap'));
