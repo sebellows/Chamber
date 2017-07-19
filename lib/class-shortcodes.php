72c4e897836ca483288ef70fb8c86511fed57b97
Joel Howard <joelhoward@gmail.com>
2017-07-19 16:04:47 -0400

    buffered shortcode output to allow content to appear in correct order

--- lib/class-shortcodes.php
+++ lib/class-shortcodes.php
@@ -12,106 +12,114 @@ namespace Chamber\Theme;
  * @link       https://github.com/sebellows/chamber
  * @license    http://www.gnu.org/licenses/gpl-3.0.html
  */
-class Shortcodes {
+class Shortcodes
+{
 
     /**
      * Register the shortcodes.
      */
-    public function __construct() {}
+    public function __construct()
+    {
+    }
 
     /**
      * Boot the shortcodes.
      */
     public function boot()
     {
-        add_shortcode('person-text', [ $this, 'person_text' ]);
-        add_shortcode('person-profile', [ $this, 'person_profile' ]);
-        add_shortcode('attraction', [ $this, 'attraction' ]);
-        add_shortcode('news-feed', [ $this, 'news']);
+        add_shortcode('person-text', [$this, 'person_text']);
+        add_shortcode('person-profile', [$this, 'person_profile']);
+        add_shortcode('attraction', [$this, 'attraction']);
+        add_shortcode('news-feed', [$this, 'news']);
     }
 
-    public function person_text($arg) {
+    public function person_text($arg)
+    {
         $this->get_data($arg['id']);
 
-        if(get_field('people_first_name')) {
+        if (get_field('people_first_name')) {
 
-            return get_template_part( 'templates/content/person' );
-        }
-        else {
+            return get_template_part('templates/content/person');
+        }else {
             return "Person not found!";
         }
     }
 
-    public function person_profile($arg) {
-        $this->get_data( $arg['id'] );
+    public function get_data($id)
+    {
+        global $post;
+        $post = wp_get_single_post($id);
 
-        if(get_field('people_first_name')) {
+        return setup_postdata($post);
+    }
 
-            return get_template_part( 'templates/content/person-text' );
-        }
-        else {
+    public function person_profile($arg)
+    {
+        $this->get_data($arg['id']);
+
+        if (get_field('people_first_name')) {
+
+            return get_template_part('templates/content/person-text');
+        }else {
             return "Person not found!";
         }
     }
 
-    public function attraction($arg) {
+    public function attraction($arg)
+    {
         $this->get_data($arg['id']);
 
-        if(get_field('attr_city')) {
+        if (get_field('attr_city')) {
 
-            return get_template_part( 'templates/content/archive', 'attraction' );
-        }
-        else {
+            return get_template_part('templates/content/archive', 'attraction');
+        }else {
             return "Attraction not found!";
         }
     }
 
     /**
      * Handles displying news articles with a shortcode by category.  defaults to five articles
+     *
      * @author Joel Howard <joelhoward@gmail.com>
      * @param $arg
      * @return string
      */
-    public function news($arg) {
+    public function news($arg)
+    {
         // set num of news items (optional)
-        if(!isset($arg['items']) || !is_numeric($arg['items'])) {
+        if (!isset($arg['items']) || !is_numeric($arg['items'])) {
             $arg['items'] = 5;
         }
 
         // check to ensure a category is set
-        if(isset($arg['category']) && term_exists($arg['category'])){
+        if (isset($arg['category']) && term_exists($arg['category'])) {
             // get our posts
             query_posts(array(
-                'category-name' => $arg['category'],
+                'category-name'  => $arg['category'],
                 'posts_per_page' => $arg['items'],
-                'post_type' => 'post',
-                'orderby' => 'id',
-                'order' => 'desc',
-                'max_num_pages' => 1
+                'post_type'      => 'post',
+                'orderby'        => 'id',
+                'order'          => 'desc',
+                'max_num_pages'  => 1
             ));
-
-            if(have_posts()) {
-                while(have_posts()) {
+            if (have_posts()) {
+                // buffer our output to be returned
+                ob_start();
+                while (have_posts()) {
                     the_post();
-                    get_template_part( 'templates/content/post' );
+                    get_template_part('templates/content/post');
                 }
                 wp_reset_postdata();
-            }
-            else {
+
+                // return buffered output from template part
+                return ob_get_clean();
+            }else {
                 return '<strong>No posts found for category:</strong> ' . $arg['category'];
             }
 
-        }
-        else {
+        }else {
             return '<strong>' . $arg['category'] . '</strong> is not a valid category.';
         }
 
-    }
-
-    public function get_data($id) {
-        global $post;
-        $post = wp_get_single_post($id);
-
-        return setup_postdata($post);
     }
 }
