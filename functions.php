475bc0abed43c36282d309737af33b82898fd6ff
Joel Howard <joelhoward@gmail.com>
2017-10-17 21:53:23 -0400

    re-ordered search results

--- functions.php
+++ functions.php
@@ -4,8 +4,8 @@ namespace Chamber\Theme;
 
 use Chamber\Theme\Theme;
 
-if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-	require __DIR__ . '/vendor/autoload.php';
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
 }
 
 global $chamber_theme;
@@ -15,6 +15,15 @@ $chamber_theme = Theme::getInstance();
 $chamber_theme->boot();
 
 # Enable shortcodes in text widgets - Joel
-add_filter( 'widget_text', 'do_shortcode' );
+add_filter('widget_text', 'do_shortcode');
 
+# re-order search results to prioritize pages > posts
+add_action('pre_get_posts', function ($query) {
+    if ($query->is_search()) {
+        $query->set('orderby', 'post_type');
+        $query->set('order', 'ASC');
+    }
+    return $query;
+
+});
 // dd(chamber('bootstrap'));
